838c01c0ee0ffb8fd01cc6cbb5e4586be64e2df9
yoshinory <53548214+yoshinory@users.noreply.github.com>
2023-11-01 23:18:04 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#!python3.11
+import streamlit as st 
+import numpy as np
+import pandas as pd 
+
+st.title ('Streamlit　入門')
+
+st.write('DataFrame')
+
+df = pd.DataFrame({
+    '1列目':[1,2,3,4],
+    '2列目':[10,20,30,40]
+})
+
+st.dataframe(df)
+
