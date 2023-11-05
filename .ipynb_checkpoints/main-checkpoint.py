5eb17f3a0e19fcbedcebf1897715764279c5beba
yoshinory <yoshi19950212@gmail.com>
2023-11-05 22:55:24 +0900

    20231105コミット

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,18 @@
+#!python3.11
+import streamlit as st 
+import numpy as np
+import pandas as pd 
+
+st.title ('Streamlit　入門')
+
+st.write('DataFrame')
+
+df = pd.DataFrame({
+    '1列目':[1,2,3,4],
+    '2列目':[10,20,30,40]
+})
+
+st.write(df)
+st.write(df)
+st.write('DataFrame')
+st.write('DataFrame')
